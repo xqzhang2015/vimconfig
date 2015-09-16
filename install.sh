c391f89b8c3c396f99501b9e8e9273ddd915f105
xqzhang <xqzhang@fwadmins-MacBook-Pro.local>
2015-09-16 23:44:49 +0800

    modify install.sh for compatibility of Mac and Linux

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 PATH_VIMCONFIG=`pwd`
-sed -i "s%PATH_VIM%${PATH_VIMCONFIG}%g" vimrc
+if [ "`uname`"x = "Linux"x ];then 
+	sed -i "s%PATH_VIM%${PATH_VIMCONFIG}%g" vimrc
+elif [ "`uname`"x = "Darwin"x ];then
+	sed -i "" "s%PATH_VIM%${PATH_VIMCONFIG}%g" vimrc
+fi
 
 cp $HOME/.vimrc $HOME/.vimrc.old
 cp vimrc $HOME/.vimrc
